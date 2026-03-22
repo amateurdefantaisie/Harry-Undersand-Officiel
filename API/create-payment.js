d8b9635a8b07b91efd5cd449980189ecac084ef2
Alice <Alicebotofficiel@gmail.com>
2026-03-22 14:31:40 +0100

    Update create-payment.js

--- API/create-payment.js
+++ API/create-payment.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const response = await fetch("https://api.sandbox.pawapay.io/v1/deposits", {
     method: "POST",
     headers: {
-      "Authorization": `Bearer ${process.env.PAWAPAY_API_KEY}`,
+      "Authorization": `Bearer ${process.env.eyJraWQiOiIxIiwiYWxnIjoiRVMyNTYifQ.eyJ0dCI6IkFBVCIsInN1YiI6IjE4MjcxIiwibWF2IjoiMSIsImV4cCI6MjA4OTgwNTQ3MCwiaWF0IjoxNzc0MTg2MjcwLCJwbSI6IkRBRixQQUYiLCJqdGkiOiJmMjc2ZjBiMS1lZWEyLTQxN2MtYTNlNC05YjUxZTJhNGJlM2IifQ.sdfLrXMARwGt1KH_cJ7FwXEz4At02G1QNNZ4VQeorY0zz_AjBnkyUG33nmEkYM-CsEYeoJYZY-4NPyJ_c9QKhg}`,
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
